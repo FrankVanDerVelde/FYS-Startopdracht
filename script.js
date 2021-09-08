ed17cef78ca73ce755d7d818ab6b727a201de74a
Frank <frankvdv1998@gmail.com>
2021-09-08 23:14:49 +0200

    Homepage almost completed

--- script.js
+++ script.js
@@ -8,11 +8,15 @@ function switchNavClass() {
 }
 
 window.onload = (event) => {
-    document.querySelectorAll('.spoiler-warning').forEach(spoilerWarningElement => {
-        spoilerWarningElement.addEventListener('click', () => {
-            spoilerWarningElement.parentElement.querySelectorAll('.spoiler-content').forEach(spoilerContent => {
-                spoilerContent.classList.remove('hidden');
-            })
-        });
+    document.querySelectorAll('.spoiler-card').forEach(spoilercard => {
+        const spoilerButton = spoilercard.querySelector('.spoiler-button');
+        spoilerButton.addEventListener('click', () => {
+            const spoilerButtonIcon = spoilerButton.querySelector('i');spoilercard.querySelector('.spoiler-content').classList.toggle('hidden');
+            if (spoilerButtonIcon.classList.contains('fa-eye-slash')) {
+                spoilerButtonIcon.classList.replace('fa-eye-slash', 'fa-eye');
+            } else {
+                spoilerButtonIcon.classList.replace('fa-eye', 'fa-eye-slash');
+            }
+        })
     })
 };
